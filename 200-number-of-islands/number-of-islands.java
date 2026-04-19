f1aa3ac491723397e1eeb72f19773b776c344b7d
CHARN KUMAR <123224577+Charan1205@users.noreply.github.com>
2026-04-19 17:33:35 +0530

    Time: 2 ms (99.88%) | Memory: 52.2 MB (62.72%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int numIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                // If we find land, it's a new island
+                if (grid[r][c] == '1') {
+                    numIslands++;
+                    // Sink the entire island using DFS
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // Base case: check for boundaries or water
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
+            return;
+        }
+
+        // Mark the current cell as visited by setting it to '0'
+        grid[r][c] = '0';
+
+        // Visit all adjacent neighbors (Up, Down, Left, Right)
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+}
